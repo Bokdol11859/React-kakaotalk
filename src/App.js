6ec2db4d346d4ef0d98a9e3c0b51844ae01464e7
Bokdol11859 <2019147551@yonsei.ac.kr>
2021-10-08 10:34:35 +0900

    .

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainPage from "./components/mainpage/componens.mainpage.mainpage";
+
+function App() {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
